6e74c5732460f193353c9b3eb235622768986c58
Merideth Safflower <msafflow@ga-t1.msk.21-school.ru>
2020-11-03 21:44:13 +0300

    final

---
+++ setup.sh
@@ -0,0 +1,32 @@
+export MINIKUBE_HOME=~/goinfre
+
+minikube start --vm-driver=virtualbox --cpus=2 --disk-size="11000mb" --memory="3000mb"
+
+eval $(minikube docker-env)
+
+minikube addons enable metallb;
+minikube addons enable metrics-server;
+minikube addons enable dashboard;
+
+kubectl apply -f srcs/metallb-config.yaml
+
+docker build -t nginx srcs/nginx/
+docker build -t php srcs/php/
+docker build -t mariadb srcs/mysql/
+docker build -t wordpress srcs/wordpress/
+docker build -t influxdb srcs/influxdb/
+docker build -t telegraf srcs/telegraf/
+docker build -t grafana srcs/grafana/
+docker build -t ftps srcs/ftps/
+
+kubectl apply -f srcs/mysql/mariadb.yaml
+kubectl apply -f srcs/nginx/nginx.yaml
+kubectl apply -f srcs/php/php.yaml
+kubectl apply -f srcs/wordpress/wordpress.yaml
+kubectl apply -f srcs/influxdb/influxdb.yaml
+kubectl apply -f srcs/telegraf/telegraf.yaml
+kubectl apply -f srcs/grafana/grafana.yaml
+kubectl apply -f srcs/ftps/ftps.yaml
+
+minikube dashboard
+
---
+++ srcs/php/start_php.sh
@@ -0,0 +1,3 @@
+php-fpm7
+nginx -g "daemon off;"
+/usr/bin/supervisord -c /etc/supervisord.conf
